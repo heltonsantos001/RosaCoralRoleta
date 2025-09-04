482e704c2421c86f35706f057ad16bb31cc745fd
helton santos <heltonsanto001@gmail.com>
2025-09-04 01:34:03 -0300

    cors

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors({
-  origin: "https://www.userosacoral.com"
-}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.userosacoral.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 
 app.use("/registra-brinde", router)
 
